d762c6f7ddad38de7128b01dc3111d3e2a641a44
S Nikhileswara Rao <nikhilproffesion@gmail.com>
2024-10-30 14:58:03 +0530

    Final web portal

---
+++ AI Calc Web/ai-calc-fe/constants.ts
@@ -0,0 +1,15 @@
+const SWATCHES = [
+    "#FFFFFF", // White
+    "#FF0000", // Red
+    "#FFC0CB", // Pink
+    "#800080", // Purple
+    "#A52A2A", // Brown
+    "#0000FF", // Blue
+    "#008000", // Green
+    "#FFFF00", // Yellow
+    "#FFA500", // Orange
+    "#39FF14", // Neon Green
+    "#E0115F"  // Royal Pink
+];
+
+export{SWATCHES};
